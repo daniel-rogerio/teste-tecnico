e8a4fd8b05a44809deb1d0dfa5158dbef13711b7
daniel-rogerio <danielr.xp@gmail.com>
2024-12-05 11:06:59 -0300

    Feat: adicionado loader ao app.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoadSpinnerComponent } from './components/load-spinner/load-spinner.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    LoadSpinnerComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
