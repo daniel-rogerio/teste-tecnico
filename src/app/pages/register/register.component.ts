a3925f20b5ac84e65a62306d164f1827cffc88e8
daniel-rogerio <danielr.xp@gmail.com>
2024-12-04 22:38:03 -0300

    Feat: criado componente e página de registro de usuários.

---
+++ src/app/pages/register/register.component.ts
@@ -0,0 +1,21 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [
+    MatCardModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule    
+  ],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class RegisterComponent {
+}
