c6ab5b0250b140cf29567a8585b34c24797024f4
daniel-rogerio <danielr.xp@gmail.com>
2024-12-04 23:18:04 -0300

    Feat: adicionado estilização à página de detalhes dos campeões.

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -2,13 +2,15 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../../components/header/header.component';
 import { MatCardModule } from '@angular/material/card';
+import { CardChampionSkinComponent } from '../../components/card-champion-skin/card-champion-skin.component';
 
 @Component({
   selector: 'app-details',
   imports: [
     CommonModule,
     HeaderComponent,
-    MatCardModule
+    MatCardModule,
+    CardChampionSkinComponent
   ],
   templateUrl: './details.component.html',
   styleUrl: './details.component.scss',
