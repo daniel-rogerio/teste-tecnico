1b938824985ec5d158b8db5695de6b11e1664369
daniel-rogerio <danielr.xp@gmail.com>
2024-12-04 22:57:26 -0300

    Feat: finalização da estilização da página de listagem de campeões.

--- src/app/pages/champions/champions.component.ts
+++ src/app/pages/champions/champions.component.ts
@@ -8,6 +8,40 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatTableModule } from '@angular/material/table';
 import { MatIcon } from '@angular/material/icon';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatButtonModule } from '@angular/material/button';
+
+export interface PeriodicElement {
+  icon: number;
+  name: string;
+  title: number;
+  info: string;
+}
+const ELEMENT_DATA: PeriodicElement[] = [
+  {icon: 1, name: 'Hydrogen', title: 1.0079, info: 'H'},
+  {icon: 2, name: 'Helium', title: 4.0026, info: 'He'},
+  {icon: 3, name: 'Lithium', title: 6.941, info: 'Li'},
+  {icon: 4, name: 'Beryllium', title: 9.0122, info: 'Be'},
+  {icon: 5, name: 'Boron', title: 10.811, info: 'B'},
+  {icon: 6, name: 'Carbon', title: 12.0107, info: 'C'},
+  {icon: 7, name: 'Nitrogen', title: 14.0067, info: 'N'},
+  {icon: 8, name: 'Oxygen', title: 15.9994, info: 'O'},
+  {icon: 9, name: 'Fluorine', title: 18.9984, info: 'F'},
+  {icon: 11, name: 'Neon', title: 20.1797, info: 'Ne'},
+  {icon: 12, name: 'Neon', title: 20.1797, info: 'Ne'},
+  {icon: 13, name: 'Neon', title: 20.1797, info: 'Ne'},
+  {icon: 14, name: 'Neon', title: 20.1797, info: 'Ne'},
+  {icon: 15, name: 'Neon', title: 20.1797, info: 'Ne'},
+  {icon: 16, name: 'Neon', title: 20.1797, info: 'Ne'},
+  {icon: 17, name: 'Neon', title: 20.1797, info: 'Ne'},
+  {icon: 18, name: 'Neon', title: 20.1797, info: 'Ne'},
+  {icon: 19, name: 'Neon', title: 20.1797, info: 'Ne'},
+  {icon: 20, name: 'Neon', title: 20.1797, info: 'Ne'},
+  {icon: 21, name: 'Neon', title: 20.1797, info: 'Ne'},
+  {icon: 22, name: 'Neon', title: 20.1797, info: 'Ne'},
+  {icon: 23, name: 'Neon', title: 20.1797, info: 'Ne'},
+  {icon: 24, name: 'Neon', title: 20.1797, info: 'Ne'},
+  {icon: 25, name: 'Neon', title: 20.1797, info: 'Ne'},
+];
 
 @Component({
   selector: 'app-champions',
@@ -20,7 +54,8 @@ import { MatPaginatorModule } from '@angular/material/paginator';
     MatFormFieldModule,
     MatTableModule,
     MatIcon,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatButtonModule
   ],
   templateUrl: './champions.component.html',
   styleUrl: './champions.component.scss',
@@ -28,4 +63,5 @@ import { MatPaginatorModule } from '@angular/material/paginator';
 })
 export class ChampionsComponent {
   displayedColumns: string[] = ['icon', 'name', 'title', 'info'];
+  dataSource = ELEMENT_DATA;
 }
