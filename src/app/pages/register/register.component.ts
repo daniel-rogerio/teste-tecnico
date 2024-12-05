8c8c129e9fbc2a1cbedb0a7dad73e0b255b3cde2
daniel-rogerio <danielr.xp@gmail.com>
2024-12-04 23:54:58 -0300

    Feat: atualização no componente de registro de usuários:  - Incluído 'ReactiveFormModule' para gerenciar o formulário;  - Função para cadastrar usuários via API.

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,8 +1,12 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UserService } from '../../services/user.service';
+import { SingUpUserRequest } from '../../interfaces/user/SingUpUserRequest';
 
 @Component({
   selector: 'app-register',
@@ -11,11 +15,36 @@ import { MatButtonModule } from '@angular/material/button';
     MatCardModule,
     MatInputModule,
     MatFormFieldModule,
-    MatButtonModule    
+    MatButtonModule,
+    RouterLink,
+    ReactiveFormsModule,
   ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RegisterComponent {
+  private formsBuilder: FormBuilder = inject(FormBuilder);
+  registerForm = this.formsBuilder.group({
+    name: ['', Validators.required],
+    birthDate: ['', Validators.required],
+    username: ['', Validators.required],
+    password: ['', Validators.required]
+  })
+  private userService: UserService = inject(UserService);
+
+  onSubmitRegisterForm(): void {
+    if (this.registerForm.value && this.registerForm.valid) {
+      this.userService.registerUser(this.registerForm.value as SingUpUserRequest)
+      .subscribe({
+        next: (response) => {
+          if (response) {
+            alert('Usuario criado com sucesso');
+            this.registerForm.reset;
+          }
+        },
+        error: (error) => console.log(error)
+      });
+    }
+  }
 }
