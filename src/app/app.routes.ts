b4759723c56cbbd1b41a58f53531558cc9eefd04
daniel-rogerio <danielr.xp@gmail.com>
2024-12-04 22:50:07 -0300

    Feat: incluído rota para a página de listagem de campeões.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { ChampionsComponent } from './pages/champions/champions.component';
 
 export const routes: Routes = [
     {path: '', component: LoginComponent},
-    {path: 'register', component: RegisterComponent}
+    {path: 'register', component: RegisterComponent},
+    {path: 'champions', component: ChampionsComponent}
 ];
