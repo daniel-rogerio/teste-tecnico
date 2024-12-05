1cc291b2807d4c5aefcb143ef2fcce1c89765384
daniel-rogerio <danielr.xp@gmail.com>
2024-12-04 23:17:43 -0300

    Feat: criado componente de card para apresentação das skins dos campeões.

---
+++ src/app/components/card-champion-skin/card-champion-skin.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-card-champion-skin',
+  imports: [MatCardModule],
+  templateUrl: './card-champion-skin.component.html',
+  styleUrl: './card-champion-skin.component.scss'
+})
+export class CardChampionSkinComponent {
+
+}
