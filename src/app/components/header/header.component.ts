008a05120b8c076e507f6878a6e2d0a23aa6bdd3
daniel-rogerio <danielr.xp@gmail.com>
2024-12-04 23:59:15 -0300

    Feat: incluído função para deslogar o usuário.

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -17,4 +19,11 @@ import { MatTooltipModule } from '@angular/material/tooltip';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  private cookieService: CookieService = inject(CookieService);
+  private router: Router = inject(Router);
+
+  userLogout(): void {
+    this.cookieService.delete('USER_COOKIE');
+    void this.router.navigate(['/login']);
+  }
 }
