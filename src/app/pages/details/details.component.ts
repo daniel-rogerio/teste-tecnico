d13fabf5737ca9e76e4144d65009f23d188c9831
daniel-rogerio <danielr.xp@gmail.com>
2024-12-04 23:25:53 -0300

    Feat: incluído o modal das skins na página de detalhes dos campeões.

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,8 +1,10 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../../components/header/header.component';
 import { MatCardModule } from '@angular/material/card';
 import { CardChampionSkinComponent } from '../../components/card-champion-skin/card-champion-skin.component';
+import { MatDialog } from '@angular/material/dialog';
+import { ImageModalComponent } from '../../components/image-modal/image-modal.component';
 
 @Component({
   selector: 'app-details',
@@ -17,5 +19,18 @@ import { CardChampionSkinComponent } from '../../components/card-champion-skin/c
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class DetailsComponent {
+  readonly dialog = inject(MatDialog);
 
+  openDialog(enterAnimationDuration: string, exitAnimationDuration: string): void {
+    this.dialog.open(ImageModalComponent, {
+      minWidth: '70vw',
+      minHeight: '80vmin',
+      enterAnimationDuration,
+      exitAnimationDuration,
+      data: {
+        title: 'Teste',
+        img: 'https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Aatrox_0.jpg'
+      }
+    });
+  }
 }
